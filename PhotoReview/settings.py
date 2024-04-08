4eaff61abc3007289f550f97dc220e4b74f19f14
EC2 Default User <ec2-user@ip-172-31-27-120.eu-west-1.compute.internal>
2024-04-08 13:53:33 +0000

    test

--- PhotoReview/settings.py
+++ PhotoReview/settings.py
@@ -135,6 +135,7 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# Test 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
